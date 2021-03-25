b297ae43c2db98355522057ee151b26fca73d2b8
Pierre <pierre1754@outlook.com>
2021-03-25 14:10:46 +0100

    import working

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,3 @@
-/*
-const mysql2 = require("mysql2");
-
-export function connectionDb() {
-  const con = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "db",
-  });
-
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-  return con;
-}
-*/
-
 import mysql from "mysql2";
 
 const connectDB = function () {
--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-/*
-
-const express = require("express");
-const connection = require("./config/db");
-const app = express();
-
-require("dotenv").config();
-const PORT = process.env.FRONT_PORT;
-
-db = connection.connectionDb();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/xD/", (req, res) => {
-  con.query("SELECT id FROM user", function (err, rows, fields) {
-    res.send(fields);
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
-});
-
-*/
-
 import express from "express";
 import con from "./config/db.js";
 import dotenv from "dotenv";
@@ -35,8 +8,6 @@ const PORT = process.env.FRONT_PORT;
 let connectDB = con();
 
 const app = express();
-
-//console.log("hello");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
